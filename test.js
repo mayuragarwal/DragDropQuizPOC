995d3a23fef08ad8b9321ee1946bdd8aa8f676bd
Mayur Agarwal <mayur.agarwal@questionmark.com>
2019-03-11 13:07:51 +0530

    changing to old javascript

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var lockedAfterDrag = false;	/* Lock items after they have been dragged, i.e. the user get"s only one shot for the correct answer */
+"use strict";
 
 var dragContent = false;
 var dragSource = false;
@@ -9,12 +9,10 @@ var dragSourceParent = false;
 var dragSourceNextSibling = false;
 var answerDivArray = new Array();
 var boxWidthArray = new Array();
-
 var containerDivPrefix = "dragContainer-";
 var dragContentContainerPrefix = "dragContentContainer-";
 var questionDivPrefix = "questionDiv-";
 var answerDivPrefix = "answerDiv-";
-
 var currentDragQuestion = null;
 
 window.onload = function () {
@@ -23,34 +21,32 @@ window.onload = function () {
 };
 
 function initDragDropScript(questionId) {
-
 	initContainerDiv(questionId);
 	initBoxWidthArray(questionId);
 	initAnswerDiv(questionId);
-
 	initAnswerDivArray(questionId);
 	initDestinationAnswerContainerArray(questionId);
 }
 
 function initContainerDiv(questionId) {
-
 	var containerDivId = containerDivPrefix + questionId;
 	var containerDiv = document.getElementById(containerDivId);
+
 	containerDiv.onmouseup = function (e) {
 		dragDropEnd(questionId);
 	};
+
 	containerDiv.onmousemove = function (e) {
 		dragDropMove(e, questionId);
 	};
 }
 
 function initBoxWidthArray(questionId) {
-
 	var containerDivId = containerDivPrefix + questionId;
 	var containerDiv = document.getElementById(containerDivId);
 	var boxes = containerDiv.getElementsByClassName("box");
-
 	var maxWidth = 0;
+
 	for (var no = 0; no < boxes.length; no++) {
 		if (maxWidth < boxes[no].clientWidth) {
 			maxWidth = boxes[no].clientWidth;
@@ -64,22 +60,23 @@ function initBoxWidthArray(questionId) {
 
 	for (var no = 0; no < boxes.length; no++) {
 		boxes[no].style.width = maxWidth + "px";
+
 		if (hasClass(boxes[no].parentElement, "questionOptionContainer")) {
-			boxes[no].parentElement.style.width = ((maxWidth * 2) + 50) + "px";
+			boxes[no].parentElement.style.width = maxWidth * 2 + 50 + "px";
 		}
 	}
 
-	containerDiv.style.width = ((maxWidth * 4) + 60) + "px";
+	containerDiv.style.width = maxWidth * 4 + 60 + "px";
 	var numOfOptions = boxes.length / 3;
-	containerDiv.style.height = ((boxes[0].clientHeight * numOfOptions) + (10 * numOfOptions)) + "px";
+	containerDiv.style.height = boxes[0].clientHeight * numOfOptions + 10 * numOfOptions + "px";
 }
 
 function initAnswerDiv(questionId) {
-
 	var answerDivId = answerDivPrefix + questionId;
 	var answerDiv = document.getElementById(answerDivId);
 	answerDiv.onselectstart = cancelEvent;
 	var divs = answerDiv.getElementsByTagName("div");
+
 	for (var no = 0; no < divs.length; no++) {
 		if (hasClass(divs[no], "dragDropAnswerBox")) {
 			divs[no].onmousedown = function (e) {
@@ -90,7 +87,6 @@ function initAnswerDiv(questionId) {
 }
 
 function initAnswerDivArray(questionId) {
-
 	var answerDivId = answerDivPrefix + questionId;
 	var answerDiv = document.getElementById(answerDivId);
 	var sourceObj = new Array();
@@ -104,11 +100,11 @@ function initAnswerDivArray(questionId) {
 }
 
 function initDestinationAnswerContainerArray(questionId) {
-
 	var questionDivId = questionDivPrefix + questionId;
 	var questionDiv = document.getElementById(questionDivId);
 	questionDiv.onselectstart = cancelEvent;
 	var divs = questionDiv.getElementsByTagName("div");
+
 	for (var no = 0; no < divs.length; no++) {
 		if (hasClass(divs[no], "destinationAnswerContainer")) {
 			var index = destinationAnswerContainerArray.length;
@@ -124,24 +120,15 @@ function initDestinationAnswerContainerArray(questionId) {
 }
 
 function startDragDrop(e, questionId) {
-
-	if (currentDragQuestion)
-		return;
-
+	if (currentDragQuestion) return;
 	currentDragQuestion = questionId;
-
 	var containerDivId = containerDivPrefix + questionId;
 	var dragContentDivId = dragContentContainerPrefix + questionId;
 
-	// if (lockedAfterDrag && e.currentTarget.parentNode.parentNode.id == questionDivId)
-	// 	return;
-
 	var dragContentDiv = document.getElementById(dragContentDivId);
 	var containerDiv = document.getElementById(containerDivId);
-
 	dragContentDiv.style.left = e.clientX + containerDiv.scrollLeft + "px";
 	dragContentDiv.style.top = e.clientY + containerDiv.scrollTop + "px";
-
 	dragSource = e.currentTarget;
 	dragSourceParent = e.currentTarget.parentNode;
 
@@ -149,26 +136,25 @@ function startDragDrop(e, questionId) {
 		// restore empty div on question side
 		dragSourceParent.innerHTML = "";
 		var emptyQuestionBoxDiv = document.createElement("div");
-		emptyQuestionBoxDiv.className = `emptyQuestionBox box ${questionId}`;
-		emptyQuestionBoxDiv.style.width = boxWidthArray.find(boxArr => boxArr["qid"] == questionId)["width"] + "px";
+		emptyQuestionBoxDiv.className = "emptyQuestionBox box ".concat(questionId);
+		emptyQuestionBoxDiv.style.width = boxWidthArray.find(function (boxArr) {
+			return boxArr["qid"] == questionId;
+		})["width"] + "px";
 		dragSourceParent.appendChild(emptyQuestionBoxDiv);
 	}
 
 	dragSourceNextSibling = false;
 	if (e.currentTarget.nextSibling) dragSourceNextSibling = e.currentTarget.nextSibling;
 	if (!dragSourceNextSibling.tagName) dragSourceNextSibling = dragSourceNextSibling.nextSibling;
-
 	dragDropTimer = 0;
 	timeoutBeforeDrag(questionId);
-
 	return false;
 }
 
 function timeoutBeforeDrag(questionId) {
-
 	if (dragDropTimer >= 0 && dragDropTimer < 10) {
 		dragDropTimer = dragDropTimer + 1;
-		setTimeout(`timeoutBeforeDrag("${questionId}")`, 10);
+		setTimeout("timeoutBeforeDrag(\"".concat(questionId, "\")"), 10);
 		return;
 	}
 
@@ -183,7 +169,6 @@ function timeoutBeforeDrag(questionId) {
 }
 
 function dragDropMove(e, questionId) {
-
 	if (dragDropTimer < 10) {
 		return;
 	}
@@ -195,47 +180,35 @@ function dragDropMove(e, questionId) {
 	var containerDivId = containerDivPrefix + questionId;
 	var dragContentDivId = dragContentContainerPrefix + questionId;
 	var dragContentDiv = document.getElementById(dragContentDivId);
-
 	var containerDivRect = document.getElementById(containerDivId).getBoundingClientRect();
 	var dragContentDivRect = dragContentDiv.getBoundingClientRect();
-
 	var maxScrollTop = containerDivRect.top + containerDivRect.height - dragContentDivRect.height;
 	var maxScrollLeft = containerDivRect.left + containerDivRect.width - dragContentDivRect.width;
-
 	dragContentDiv.style.left = Math.min(e.clientX, maxScrollLeft) + "px";
 	dragContentDiv.style.top = Math.min(e.clientY, maxScrollTop) + "px";
-
 	trySetDestinationForDrop(e, questionId);
-
 	return false;
 }
 
 function trySetDestinationForDrop(e, questionId) {
-
 	destination = false;
 	var objFound = false;
-
 	var dragWidth = dragSource.offsetWidth;
 	var dragHeight = dragSource.offsetHeight;
-
 	var mouseX = e.clientX;
 	var mouseY = e.clientY;
 
 	for (var no = 0; no < destinationAnswerContainerArray.length; no++) {
-		if (destinationAnswerContainerArray[no]["qid"] != questionId)
-			continue;
-
+		if (destinationAnswerContainerArray[no]["qid"] != questionId) continue;
 		var left = destinationAnswerContainerArray[no]["left"];
 		var top = destinationAnswerContainerArray[no]["top"];
 		var width = destinationAnswerContainerArray[no]["width"];
 		var height = destinationAnswerContainerArray[no]["height"];
-
 		var subs = destinationAnswerContainerArray[no]["obj"].getElementsByTagName("div");
+
 		// Check if question destination is suitable for dropping answer div
 		if (!objFound && subs.length == 1 && hasClass(subs[0], "emptyQuestionBox")) {
-			if (mouseX < (left / 1 + width / 1) && (mouseX + dragWidth / 1) > left
-				&& mouseY < (top / 1 + height / 1) && (mouseY + dragHeight / 1) > top) {
-
+			if (mouseX < left / 1 + width / 1 && mouseX + dragWidth / 1 > left && mouseY < top / 1 + height / 1 && mouseY + dragHeight / 1 > top) {
 				destination = destinationAnswerContainerArray[no]["obj"];
 				objFound = true;
 				break;
@@ -246,17 +219,18 @@ function trySetDestinationForDrop(e, questionId) {
 	// if question destination is not found and mouse is moved inside answer div,
 	// then user is trying to drop in answer div back. Set destination to answer div. 
 	if (!objFound) {
-		var sourceObj = answerDivArray.find(obj => obj["qid"] == questionId);
+		var sourceObj = answerDivArray.find(function (obj) {
+			return obj["qid"] == questionId;
+		});
 		var left = sourceObj["left"];
 		var top = sourceObj["top"];
 		var width = sourceObj["width"];
 		var height = sourceObj["height"];
 
-		if (mouseX < (left / 1 + width / 1) && (mouseX + dragWidth / 1) > left && mouseY < (top / 1 + height / 1)
-			&& (mouseY + dragHeight / 1) > top) {
-
+		if (mouseX < left / 1 + width / 1 && mouseX + dragWidth / 1 > left && mouseY < top / 1 + height / 1 && mouseY + dragHeight / 1 > top) {
 			// get an available answerOptionContainer
 			var destinations = sourceObj["obj"].getElementsByClassName("answerOptionContainer");
+
 			for (var no = 0; no < destinations.length; no++) {
 				if (destinations[no].getElementsByTagName("div").length == 0) {
 					destination = destinations[no];
@@ -268,7 +242,6 @@ function trySetDestinationForDrop(e, questionId) {
 }
 
 function dragDropEnd(questionId) {
-
 	if (dragDropTimer < 10) {
 		dragDropTimer = -1;
 		return;
@@ -276,21 +249,22 @@ function dragDropEnd(questionId) {
 
 	var dragContentDivId = dragContentContainerPrefix + questionId;
 	var dragContentDiv = document.getElementById(dragContentDivId);
-
 	dragContentDiv.style.display = "none";
+
 	if (destination) {
 		if (hasClass(destination, "destinationAnswerContainer")) {
 			destination.innerHTML = "";
 		}
+
 		destination.appendChild(dragSource);
 	} else {
 		if (dragSourceNextSibling) {
 			dragSourceNextSibling.parentNode.insertBefore(dragSource, dragSourceNextSibling);
-		}
-		else {
+		} else {
 			if (hasClass(dragSourceParent, "destinationAnswerContainer")) {
 				dragSourceParent.innerHTML = "";
 			}
+
 			dragSourceParent.appendChild(dragSource);
 		}
 	}
@@ -303,20 +277,24 @@ function dragDropEnd(questionId) {
 }
 
 function getTopPos(inputObj) {
-
-	if (!inputObj || !inputObj.offsetTop)
-		return 0;
+	if (!inputObj || !inputObj.offsetTop) return 0;
 	var returnValue = inputObj.offsetTop;
-	while ((inputObj = inputObj.offsetParent) != null) returnValue += inputObj.offsetTop;
+
+	while ((inputObj = inputObj.offsetParent) != null) {
+		returnValue += inputObj.offsetTop;
+	}
+
 	return returnValue;
 }
 
 function getLeftPos(inputObj) {
-
-	if (!inputObj || !inputObj.offsetLeft)
-		return 0;
+	if (!inputObj || !inputObj.offsetLeft) return 0;
 	var returnValue = inputObj.offsetLeft;
-	while ((inputObj = inputObj.offsetParent) != null) returnValue += inputObj.offsetLeft;
+
+	while ((inputObj = inputObj.offsetParent) != null) {
+		returnValue += inputObj.offsetLeft;
+	}
+
 	return returnValue;
 }
 
@@ -325,21 +303,18 @@ function cancelEvent() {
 }
 
 function hasClass(el, className) {
-	if (el.classList)
-		return el.classList.contains(className);
+	if (el.classList) return el.classList.contains(className);
 	return !!el.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
 }
 
 function addClass(el, className) {
-	if (el.classList)
-		el.classList.add(className)
+	if (el.classList) el.classList.add(className);
 	else if (!hasClass(el, className))
 		el.className += " " + className;
 }
 
 function removeClass(el, className) {
-	if (el.classList)
-		el.classList.remove(className)
+	if (el.classList) el.classList.remove(className);
 	else if (hasClass(el, className)) {
 		var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
 		el.className = el.className.replace(reg, " ");
